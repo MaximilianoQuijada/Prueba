26add06cefc115369c8a327511b2e0967b1a74cc
Maximiliano Quijada <mbquijada@gmail.com>
2024-02-03 12:51:07 -0300

    se agrega algunas funciones JavaScrip

--- assets/js/js1.js
+++ assets/js/js1.js
@@ -1,4 +1,17 @@
 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	return new bootstrap.tooltip(tooltipTriggerEl)
+} )
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+$("button").click(function (e) {
+	e.preventDefault();
+	alert("Correo enviado exitosamente");
+});
